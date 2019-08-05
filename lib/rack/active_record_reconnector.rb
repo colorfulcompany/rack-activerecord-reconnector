4eecd136d7579349d52ff557c0ef138959049db7
T.Watanabe <watanabe@colorfulcompany.co.jp>
2019-08-05 17:14:16 +0900

    add logger and debug options for logging before re-raise exception

--- lib/rack/active_record_reconnector.rb
+++ lib/rack/active_record_reconnector.rb
@@ -1,12 +1,36 @@
 require "rack/active_record_reconnector/version"
+require "logger"
+
+require 'pry-byebug'
+
 
 module Rack
   class ActiveRecordReconnector
     #
     # [param] Rack::Application app
+    # [param] Hash options
     #
-    def initialize(app)
-      @app = app
+    def initialize(app, options = {})
+      @app         = app
+      @log_level ||= detect_log_level(options)
+      if options.has_key?(:logger) && !@log_level.nil?
+        @logger  ||= options[:logger]
+      end
+    end
+    attr_reader :logger, :log_level
+
+    #
+    # [param]  Hash options
+    # [return] Symbol or nil
+    #
+    def detect_log_level(options = {})
+      if options.has_key? :log_level
+        options[:log_level]
+      else
+        if options.has_key? :debug
+          options[:debug] ? :debug : nil
+        end
+      end
     end
 
     #
@@ -17,6 +41,7 @@ module Rack
       begin
         @app.call(env)
       rescue Exception => e
+        logger.send(log_level, "ActiveRecord reconnect ! by #{e.inspect} from #{e.backtrace.first}") if logger
         ::ActiveRecord::Base.connection.reconnect!
         raise e
       end
--- spec/rack-activerecord-reconnector_spec.rb
+++ spec/rack-activerecord-reconnector_spec.rb
@@ -35,12 +35,55 @@ describe Rack::ActiveRecordReconnector do
     before {
       mock(::ActiveRecord::Base.connection).reconnect!.once { ::ActiveRecord::Base.connection }
     }
-    it {
-      begin
-        app_with_exception.call({})
-      rescue StandardError => e
-        RR.verify
-      end
-    }
+    describe 'reconnect! called once' do
+      it {
+        begin
+          app_with_exception.call({})
+        rescue StandardError => e
+          RR.verify
+        end
+      }
+    end
+    describe 'debug logging' do
+      let(:app_with_debug) {
+        Rack::Builder.new do
+          use Rack::ActiveRecordReconnector, debug: true, logger: Logger.new(STDOUT, level: Logger::DEBUG)
+          run lambda {|env| raise StandardError}
+        end
+      }
+      it {
+        begin
+          app_with_debug.call({})
+        rescue StandardError => e
+          RR.verify
+        end
+      }
+    end
+  end
+
+  describe '#detect_log_level' do
+    describe '{:debug => true, :detect_log_level => nil}' do
+      it {
+        assert {
+          :debug == Rack::ActiveRecordReconnector.new(proc {}).detect_log_level({:debug => true})
+        }
+      }
+    end
+
+    describe '{:debug => true, :detect_log_level => :warn}' do
+      it {
+        assert {
+          :warn == Rack::ActiveRecordReconnector.new(proc {}).detect_log_level({:debug => true, :log_level => :warn})
+        }
+      }
+    end
+
+    describe 'nothing' do
+      it {
+        assert {
+          Rack::ActiveRecordReconnector.new(proc {}).detect_log_level().nil?
+        }
+      }
+    end
   end
 end
